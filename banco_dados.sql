47a00870bc1cd9fdb07f78e2ffac9e9ef2513be2
zclaro <126823565+zclaro@users.noreply.github.com>
2023-03-21 23:58:26 -0300

    Update banco_dados.sql

--- banco_dados.sql
+++ banco_dados.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `financeiro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `financeiro` ;
 
 USE `financeiro`;
 
